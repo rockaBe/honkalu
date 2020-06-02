11068ab57c15e666da70200888aec3e7ebd81c87
Bernd Naehler <bnaehler@babbel.com>
2020-06-02 09:47:35 +0200

    feat: add dynamic page list content

--- pages/blog.js
+++ pages/blog.js
@@ -1,6 +1,21 @@
+import Link from 'next/link'
+import posts from '../posts.json'
+
 const Blog = () => (
   <div>
-    <h1>Blog-A-Lu</h1>
+    <h1>Blog</h1>
+
+    <ul>
+      {Object.entries(posts).map((value, index) => {
+        return (
+          <li key={index}>
+            <Link href='/blog/[id]' as={'/blog/' + value[0]}>
+              <a>{value[1].title}</a>
+            </Link>
+          </li>
+        )
+      })}
+    </ul>
   </div>
 )
 
